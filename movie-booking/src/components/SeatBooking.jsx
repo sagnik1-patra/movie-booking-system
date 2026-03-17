7ae9b3078c456cd7af5eb6e467b1b026eae92c61
Satyam Prakash <satyamprakash996@gmail.com>
2026-03-17 16:22:39 +0530

    feat: implement seat occupancy check and booking validation in SeatBooking, and optimize BookingHistory useEffect

--- movie-booking/src/components/SeatBooking.jsx
+++ movie-booking/src/components/SeatBooking.jsx
@@ -1,15 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import '../App.css'
 
 function SeatBooking({ movie, goBack }) {
 
   const seats = Array.from({length: 20}, (_,i)=>i+1);
 
   const [selectedSeats, setSelectedSeats] = useState([]);
+  const [bookedSeat, setBookedSeat] = useState([])
 
+  useEffect(()=>{
+    const stored = JSON.parse(localStorage.getItem("bookings"))|| []
+    
+    const currentMovieBookings = stored.filter(b => b.movie === movie.name);
+    const taken = currentMovieBookings.flatMap(b=>b.seats)
+    setBookedSeat(taken)
+  },[movie.name])
   const toggleSeat = (seat) => {
-
+    if(bookedSeat.includes(seat)) return;
     if(selectedSeats.includes(seat)){
-      setSelectedSeats(selectedSeats.filter(s=>s!==seat));
+      setSelectedSeats(selectedSeats.filter(s => s !== seat));
     } else {
       setSelectedSeats([...selectedSeats, seat]);
     }
@@ -17,6 +26,10 @@ function SeatBooking({ movie, goBack }) {
   };
 
   const confirmBooking = () => {
+    if(selectedSeats.length === 0){
+      alert("Please select atleast one seat")
+      return 
+    }
 
     const booking = {
       movie: movie.name,
@@ -41,17 +54,22 @@ function SeatBooking({ movie, goBack }) {
 
       <div className="seats">
 
-        {seats.map(seat => (
+        {seats.map(seat => {
+          const isBooked = bookedSeat.includes(seat)
+          const isSelected = selectedSeats.includes(seat)
+        return(
 
           <button
             key={seat}
-            className={selectedSeats.includes(seat) ? "selected" : ""}
+            className={`${isBooked?"booked":""} ${isSelected? "selected":""}`}
             onClick={()=>toggleSeat(seat)}
+            disabled={isBooked}
           >
             {seat}
+            
           </button>
 
-        ))}
+        )})}
 
       </div>
 
